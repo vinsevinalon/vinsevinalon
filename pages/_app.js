432247fa0e6974e47d33d2f3f6764ae9d0a0134e
Vinse Vinalon <vinsevinalon@gmail.com>
2024-07-04 08:20:47 +0800

    Implement speedinsights

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
     return (
